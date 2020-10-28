0c5ffc928baeb98bd9b6a36e049cc5d1ba5d7dc1
Harsh Soni <soniharsh020@gmail.com>
2020-10-29 00:53:10 +0530

    Enable user details url

--- medical/users/urls.py
+++ medical/users/urls.py
@@ -11,6 +11,6 @@ app_name = "users"
 urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
-    # path("<str:username>/", view=user_detail_view, name="detail"),
+    path("<str:username>/", view=user_detail_view, name="detail"),
     path("details/", view=UserProfileDetailView.as_view(), name="user_details")
 ]
