1dbdb8b72462ba906b45b8685581874cc4a79f79
Harsh Soni <devilincarcerated020@yahoo.com>
2020-11-11 23:17:55 +0530

    nit

--- config/settings/base.py
+++ config/settings/base.py
@@ -170,7 +170,7 @@ STATIC_ROOT = str(ROOT_DIR / "staticfiles")
 STATIC_URL = "/static/"
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [str(APPS_DIR / "static"),
-                    os.path.join(str(REACT_APP_DIR.path("build")), 'static'),]
+                    os.path.join(str(REACT_APP_DIR.path("build")), 'static'), ]
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
